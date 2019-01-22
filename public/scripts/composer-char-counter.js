07124b3b890aecb327b57e5f522166ed71b29d57
Jason <jason.jiron@gmail.com>
2019-01-22 23:07:51 +0000

    counter reverts back to black if < 140 and red if > 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
-  console.log('this is your script file here');
+console.log('this is your script file here');
 
-  let textArea = ".new-tweet textarea";
-  let counter = ".new-tweet .counter";
+let textArea = ".new-tweet textarea";
+let counter = ".new-tweet .counter";
 
-  $(textArea).keyup(function() {
-    let inputLength = this.value.length;
-    $(counter)[0].innerText = 140 - inputLength
-    if (inputLength > 140) {
-      $(counter).css('color', 'red')
-    }
-  })
+$(textArea).keyup(function() {
+  let inputLength = this.value.length;
+  $(counter)[0].innerText = 140 - inputLength
+  if (inputLength > 140) {
+    $(counter).css('color', 'red')
+  } else if (inputLength < 140) {
+    $(counter).css('color', 'black')
+  }
+})
 
-});
 
 
 //  minus (this.placeholder.length)
